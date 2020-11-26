514b19692ca3463d18189958dbcc5c896f3a3841
stanislav loktev <stanislav.loktev@aleskeroff.ru>
2020-11-26 17:05:57 +0300

    push

---
+++ Assets/Charecter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charecter : MonoBehaviour
+{
+	public float FlySpeed = 1f;
+	public GameObject camera;
+
+	private Vector2 targetDirection;
+	public Vector2 clampInDegrees = new Vector2(360, 180);
+	public float sensitivity = 10f;
+    public float maxYAngle = 80f;
+    private Vector3 currentRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentRotation = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") != 0)
+	    {
+	        //camera.transform.Translate(Vector3.forward * FlySpeed * Input.GetAxis("Vertical"));
+	        transform.Translate(Vector3.forward * FlySpeed * Input.GetAxis("Vertical"));
+	    }
+	   
+	   
+	    if (Input.GetAxis("Horizontal") != 0)
+	    {
+	        //camera.transform.Translate(Vector3.right * FlySpeed * Input.GetAxis("Horizontal"));
+	        transform.Translate(Vector3.right * FlySpeed * Input.GetAxis("Horizontal"));
+	    }
+
+	    //ensure these stay this way
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+ 
+/*        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        var targetOrientation = Quaternion.Euler(targetDirection);
+ 
+
+        var xRotation = Quaternion.AngleAxis(-mouseDelta.y, Vector3.right);
+        camera.transform.localRotation *= xRotation * targetOrientation;
+        var yRotation = Quaternion.AngleAxis(mouseDelta.x, transform.InverseTransformDirection(Vector3.up));
+        camera.transform.localRotation *= yRotation;
+*/
+
+        currentRotation.x += Input.GetAxis("Mouse X");
+        currentRotation.y -= Input.GetAxis("Mouse Y");
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
+        //camera.transform.rotation = Quaternion.Euler(currentRotation.y,currentRotation.x,0);
+        transform.rotation = Quaternion.Euler(currentRotation.y,currentRotation.x,0);
+    }
+}
---
+++ Assets/Game.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable] public class Player
+{
+	public string address;
+	public int port;
+	public long last_time;
+	public bool leave;
+	public Vector3 position;
+	private GameObject _object;
+	public Player(){}
+	public Player(Vector3 _p){ position = _p; }
+	public void Create(GameObject _o){ _object = _o; }
+	public void Update(){ if(_object != null) _object.transform.localPosition = position; }
+}
+public class Answer
+{
+	public int msgid;
+	public int id_player;
+	public Player[] players;
+	public Answer(){ players = new Player[]{}; }
+}
+public class Game : MonoBehaviour
+{
+    Thread readThread;
+    UdpClient client;
+
+    public int port = 22023;
+    public string ip = "78.24.222.166";
+    public GameObject templatePlayer;
+    private GameObject scene;
+    private int id_player = 0;
+    private GameObject _player;
+    private Player player;
+    private List<Player> players;
+    private bool isConnected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	players = new List<Player>();
+
+    	scene = GameObject.Find("Game").gameObject;
+    	_player = GameObject.Find("Player").gameObject;
+    	player = new Player(_player.transform.localPosition);
+
+    	client = new UdpClient(ip, port);
+
+        // create thread for reading UDP messages
+        readThread = new Thread(new ThreadStart(ReceiveData));
+        readThread.IsBackground = true;
+        readThread.Start();
+
+        string json = JsonUtility.ToJson(player);
+        byte[] sendBytes = new UTF8Encoding(true).GetBytes("{\"msgid\":10001, \"player\":"+json+"}");
+	    client.Send(sendBytes, sendBytes.Length);
+    }
+	// Unity Application Quit Function
+    void OnApplicationQuit()
+    {
+        stopThread();
+    }
+    // Stop reading UDP messages
+    private void stopThread()
+    {
+        if (readThread.IsAlive)
+        {
+            readThread.Abort();
+        }
+        client.Close();
+        isConnected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	for(int id = 0; id < players.Count; id++)
+    		players[id].Update();
+
+        //player.position = _player.transform.localPosition;
+        if(isConnected)
+        {
+
+         	string json = JsonUtility.ToJson(player);
+        	byte[] sendBytes = new UTF8Encoding(true).GetBytes("{\"msgid\":10002, \"id_player\":"+id_player.ToString()+",\"player\":"+json+"}");
+        	client.Send(sendBytes, sendBytes.Length);
+        	sendBytes = new UTF8Encoding(true).GetBytes("{\"msgid\":10003}");
+	    	client.Send(sendBytes, sendBytes.Length);     	
+        }
+    }
+
+        // receive thread function
+    private void ReceiveData()
+    {
+        while (true)
+        {
+            try
+            {
+                // receive bytes
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                // encode UTF8-coded bytes to text format
+                string text = new UTF8Encoding(true).GetString(data);
+
+                // show received message
+                print(">> " + text);
+
+                Answer answer = JsonUtility.FromJson<Answer>(text);
+                switch(answer.msgid)
+                {
+                	case 10001:
+                	{
+                		id_player = answer.id_player;
+                		for(int id = 0; id < answer.players.Length; id++)
+                		{
+                			players.Add(new Player(answer.players[id].position));
+                			players[players.Count - 1].Create(Instantiate(templatePlayer, scene.transform, false).gameObject);
+                		}
+
+                		isConnected = true;
+                	}
+                	break;
+
+                	case 10002:
+                	{
+                		players.Add(new Player(answer.players[0].position));
+                		players[players.Count - 1].Create(Instantiate(templatePlayer, scene.transform, false).gameObject);
+                	}
+                	break;
+
+                	case 10003:
+                	{
+                		players[answer.id_player].position = answer.players[0].position;
+                	}
+                	break;
+                }
+
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+}
