28c04a14de29f39c82d1c3cd12e51b77f1c88216
stanislav loktev <stanislav.loktev@aleskeroff.ru>
2020-12-02 18:02:08 +0300

    push

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 [Serializable] public class Object{
 	public string name;
 	public Vector3 position;
@@ -24,53 +23,61 @@ using UnityEngine.UI;
         this.rotation = _o.rotation;
         this.velocity = _o.velocity;
     }
+    public void Set(Object _o)
+    {
+    	this.name = _o.name;
+        this.position = _o.position;
+        this.rotation = _o.rotation;
+        this.velocity = _o.velocity;
+    }
 }
 
 [Serializable] public class Player
 {
-	public string address;
-	public int port;
-	public long last_time;
-	public bool leave;
+	public string ip;
+	public long lastTimeServer;
 
-	private GameObject _object;
+	private bool exit;
+	private GameObject player;
+	private TextMesh name;
+
     public Object transform;
-    public float syncTime;
-    public float syncDelay;
 
-    public bool isAdd(){ return _object != null ? true : false; }
+    public bool isAdd(){ return player != null ? true : false; }
 	public Player(){ transform = new Object(); }
 	public Player(Object _o){ transform = _o; }
-	public void Create(GameObject _o){ _object = _o; }
+	public void Create(GameObject _o)
+	{ 
+		player = _o;
+		name = _o.transform.GetChild(0).GetComponent<TextMesh>();
+	}
 	public void Update()
 	{
         if (_object == null) return;
-		if(leave) GameObject.Destroy(_object);
+		if(exit) GameObject.Destroy(_object);
 
-        _object.transform.localPosition = Vector3.Lerp(_object.transform.localPosition, transform.position, 1);
-	 	_object.transform.rotation = Quaternion.Lerp(_object.transform.rotation, Quaternion.Euler(transform.rotation), 1);
-	 	_object.transform.GetChild(0).GetComponent<TextMesh>().text = transform.name;
+        player.transform.localPosition = Vector3.Lerp(player.transform.localPosition, transform.position, 1);
+	 	player.transform.rotation = Quaternion.Lerp(player.transform.rotation, Quaternion.Euler(transform.rotation), 1);
+	 	name.text = transform.name;
 	}
+	public void Exit(){ this.exit = true; }
+
 }
-public class Answer
+public class Response
 {
 	public int msgid;
-	public int id_player;
-	public Player[] players;
-    public Object[] objects;
-    public Answer(){ players = new Player[]{}; }
 }
-
-public class AnswerObject
+public class ResponseObjects
+{
+	public int msgid;
+	public List<Player> players;
+    public Response(){ players = new List<Player>(); }
+}
+public class ResponseObject
 {
     public int msgid;
-    public Object objectSync;
-    public AnswerObject() { objectSync = new Object(); }
-    public AnswerObject(AnswerObject _o)
-    {
-        this.msgid = _o.msgid;
-        this.objectSync = new Object(_o.objectSync);
-    }
+    public Player player;
+    public ResponseObject() { player = new Player(); }
 }
 
 public class Game : MonoBehaviour
@@ -78,18 +85,32 @@ public class Game : MonoBehaviour
     Thread readThread;
     public UdpClient client;
 
-    public int port = 22023;
-    public string ip = "78.24.222.166";
-    public GameObject templatePlayer;
-    public InputField name;
+    [HideInInspector] public bool isConnected = false;
+
+    public int Port = 22023;
+    public string IP = "78.24.222.166";
+    public GameObject TemplatePlayer;
+    public InputField Name;
+
     private GameObject scene;
-    public int id_player = 0;
-    private GameObject _player;
-    private Object player;
+    private GameObject player;
+    private Object playerSync;
+
     private List<Player> players;
-    private List<AnswerObject> answers;
-    private List<Object> objects;
-    public bool isConnected = false;
+
+    public void SaveName()
+    {
+    	playerSync.name = name.text;
+    	player.transform.GetChild(1).GetComponent<TextMesh>().text = playerSync.name;
+    }
+
+    void Send(string json, bool secure)
+    {
+    	if(secure)
+    		if (!isConnected) return;
+    	byte[] sendBytes = new UTF8Encoding(true).GetBytes(json);
+	    client.Send(sendBytes, sendBytes.Length);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -112,77 +133,20 @@ public class Game : MonoBehaviour
         readThread = new Thread(new ThreadStart(ReceiveData));
         readThread.IsBackground = true;
         readThread.Start();
+    }
 
-        string json = JsonUtility.ToJson(player);
-        byte[] sendBytes = new UTF8Encoding(true).GetBytes("{\"msgid\":10001, \"player\":"+json+"}");
-	    client.Send(sendBytes, sendBytes.Length);
-    }
-	// Unity Application Quit Function
-    void OnApplicationQuit()
-    {
-        stopThread();
-    }
-    // Stop reading UDP messages
-    private void stopThread()
-    {
-        if (readThread.IsAlive)
-        {
-            readThread.Abort();
-        }
-        client.Close();
-        isConnected = false;
-    }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-    	for(int id = 0; id < players.Count; id++)
-    	    if(players[id].isAdd())
-    		    players[id].Update();
-    	    else if(!players[id].leave) players[id].Create(Instantiate(templatePlayer, scene.transform, false).gameObject);
-
-        for (int id = 0; id < objects.Count; id++)
+        if (!isConnected)
         {
-            try
-            {
-                print(objects[id].name);
-                GameObject.Find(objects[id].name).gameObject.GetComponent<SyncServerScene>().UpdateServer(objects[id], true);
-            }
-            catch (Exception err)
-            {
-                print(err.ToString());
-            }
-        }
-        for (int id = 0; id < answers.Count; id++)
-        {
-            try
-            {
-                print(answers[id].objectSync.name);
-                GameObject.Find(answers[id].objectSync.name).gameObject.GetComponent<SyncServerScene>().UpdateServer(answers[id].objectSync);
-            }
-            catch (Exception err)
-            {
-                print(err.ToString());
-            }
-        }
-
-        objects.Clear();
-        answers.Clear();
-
-        if (isConnected)
-        {
-            player.position = _player.transform.localPosition;
-            player.rotation = _player.transform.eulerAngles;
-
-         	string json = JsonUtility.ToJson(player);
-        	byte[] sendBytes = new UTF8Encoding(true).GetBytes("{\"msgid\":10002, \"id_player\":"+id_player.ToString()+",\"player\":"+json+"}");
-        	client.Send(sendBytes, sendBytes.Length);
-        	sendBytes = new UTF8Encoding(true).GetBytes("{\"msgid\":10003}");
-	    	client.Send(sendBytes, sendBytes.Length);     	
+        	string request = "{\"msgid\":10000}";
+	    	Send(request, false);  	
         }
     }
 
-        // receive thread function
+    // receive thread function
     private void ReceiveData()
     {
         while (true)
@@ -196,74 +160,38 @@ public class Game : MonoBehaviour
                 // encode UTF8-coded bytes to text format
                 string text = new UTF8Encoding(true).GetString(data);
 
-                Answer answer = JsonUtility.FromJson<Answer>(text);
-                switch(answer.msgid)
+                Response response = JsonUtility.FromJson<Response>(text);
+
+                switch(response.msgid)
                 {
                 	//Connected user
-                	case 10001:
+                	case 10000:
                 	{
-                		id_player = answer.id_player;
-                		for(int id = 0; id < answer.players.Length; id++)
-                		{
-                			Object _o = new Object();
-					    	_o.position = answer.players[id].transform.position;
-					    	_o.rotation = answer.players[id].transform.rotation;
-					    	_o.name = answer.players[id].transform.name;
-                			players.Add(new Player(_o));
-                		}
-                        for (int id = 0; id < answer.objects.Length; id++)
-                            objects.Add(new Object(answer.objects[id]));
                 		isConnected = true;
-
-                		// show received message
-                		//print(">> " + text);
                 	}
                 	break;
-
-                	//Connected guest
-                	case 10002:
+                	case 10001: //add player
                 	{
-                		// show received message
-                		//print(">> " + text);
-            			Object _o = new Object();
-				    	_o.position = answer.players[0].transform.position;
-				    	_o.rotation = answer.players[0].transform.rotation;
-				    	_o.name = answer.players[0].transform.name;
-                		players.Add(new Player(_o));
+                		ResponseObject response_object = JsonUtility.FromJson<ResponseObject>(text);
+                		players.Add(response_object.player);
                 	}
                 	break;
-
-                	//Update guest
-                	case 10003:
+                	case 10002: //remove player
                 	{
-                		// show received message
-                		//print(">> " + text);
-
-                		if(id_player < answer.id_player)
-                		{
-                            players[answer.id_player - 1].transform.name = answer.players[0].transform.name;
-               			    players[answer.id_player - 1].transform.position = answer.players[0].transform.position;
-                			players[answer.id_player - 1].transform.rotation = answer.players[0].transform.rotation;
-                			players[answer.id_player - 1].leave = answer.players[0].leave;
-                		}
-                		else{
-                			players[answer.id_player].transform.name = answer.players[0].transform.name;
-                			players[answer.id_player].transform.position = answer.players[0].transform.position;
-                			players[answer.id_player].transform.rotation = answer.players[0].transform.rotation; 
-                			players[answer.id_player].leave = answer.players[0].leave;
-                		}
+                		ResponseObject response_object = JsonUtility.FromJson<ResponseObject>(text);
+                		int id = players.IndexOf(response_object.player);
+                		if(id > -1)
+                			players[id].Exit();
                 	}
                 	break;
-                    //Update
-                    case 10004:
-                        {
-                            AnswerObject answerObj = JsonUtility.FromJson<AnswerObject>(text);
-                            // show received message
-                            //print(">> " + text);
-                            //print(">> " + answerObj.objectSync.name);
-                            answers.Add(new AnswerObject(answerObj));
-                        }
-                    break;
+                	case 10003: //update player
+                	{
+                		ResponseObject response_object = JsonUtility.FromJson<ResponseObject>(text);
+                		int id = players.IndexOf(response_object.player);
+                		if(id > -1)
+                			players[id].transform.Set(response_object.player.transform);
+                	}
+                	break;
                 }
 
             }
@@ -274,9 +202,22 @@ public class Game : MonoBehaviour
         }
     }
 
-    public void SaveName()
+
+
+    // Unity Application Quit Function
+    void OnApplicationQuit()
     {
-    	player.name = name.text;
-    	_player.transform.GetChild(1).GetComponent<TextMesh>().text = player.name;
+        stopThread();
+    }
+
+    // Stop reading UDP messages
+    private void stopThread()
+    {
+        if (readThread.IsAlive)
+        {
+            readThread.Abort();
+        }
+        client.Close();
+        isConnected = false;
     }
 }
---
+++ Assets/GameServer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable] public class Client{
+    public string ip;
+    public long lastTimeServer;
+    private EndPoint point;
+    public Object transform;
+    public Client(EndPoint _p){ 
+        point = _p; 
+        ip = ((IPEndPoint)_p).Address.ToString(); 
+        transform = new Object();
+        transform.name = ip;
+    }
+    public void Send(Socket server, string json)
+    {
+        byte[] sendBytes = new UTF8Encoding(true).GetBytes(json);
+        server.SendTo(sendBytes, point);
+    }
+}
+
+public class GameServer : MonoBehaviour
+{
+    Thread serverThread;
+
+    private Socket server;
+    private int port = 22023;
+    private List<Client> clients;
+    private GameObject scene;
+
+    private struct Request{
+        public int msgid;
+    }
+
+    void Start()
+    {
+        scene = GameObject.Find("Game").gameObject;
+
+        clients = new List<Client>();
+
+        server = new Socket(IPAddress.Any.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint pointIP = new IPEndPoint(IPAddress.Any, port);
+        
+        try {
+            server.Bind(pointIP);
+        }
+        catch (Exception e) {
+            print("server error: " + e.ToString());
+        }
+
+        // create thread for reading UDP messages
+        serverThread = new Thread(new ThreadStart(ReceiveData));
+        serverThread.IsBackground = true;
+        serverThread.Start();
+    }
+
+	// Unity Application Quit Function
+    void OnApplicationQuit()
+    {
+        stopThread();
+    }
+
+    // Stop reading UDP messages
+    private void stopThread()
+    {
+        if (serverThread.IsAlive)
+        {
+            serverThread.Abort();
+        }
+        server.Close();
+    }
+
+    // receive thread function
+    private void ReceiveData()
+    {
+        while (true)
+        {
+            try
+            {
+                byte[] data = new byte[1024];
+
+                // Creates an IPEndPoint to capture the identity of the sending host.
+                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint senderRemote = (EndPoint)sender;
+
+                server.ReceiveFrom(data, ref senderRemote);
+                // encode UTF8-coded bytes to text format
+                string text = new UTF8Encoding(true).GetString(data);
+
+                Request request = JsonUtility.FromJson<Request>(text);
+                string response = "";
+
+                switch(request.msgid)
+                {
+                    case 10000:
+                    {
+                        Client client = new Client(senderRemote);
+                        if(clients.IndexOf(client) > -1) 
+                        {
+                            print("Client already added -> " + ((IPEndPoint)senderRemote).Address.ToString());
+                            break;
+                        }
+                        
+                        string json_clients = JsonUtility.ToJson(clients);
+                        response = "{\"msgid\":10000, \"clients\":\""+json_clients+"\"}";
+                        client.Send(server, response);
+
+                        for(int id = 0; id < clients.Count; id++)
+                        {
+                            string json_client = JsonUtility.ToJson(client);
+                            response = "{\"msgid\":10001, \"client\":\""+json_client+"\"}";
+                            clients[id].Send(server, response);
+                        }
+
+                        clients.Add(client);
+                        print("Add client -> " + ((IPEndPoint)senderRemote).Address.ToString());
+                    }
+                    break;
+                    case 20000:
+                    {
+                        Client client = new Client(senderRemote);
+                        int id = clients.IndexOf(client);
+                        if(id > -1)
+                            clients[id].lastTimeServer = Time.time;
+                    }
+                    break;
+                }
+
+                for(int id = 0; id < clients.Count; id++)
+                {
+                    if(clients[id].lastTimeServer < Time.time + 3000)
+                        clients.Remove(client);
+                        for(int id = 0; id < clients.Count; id++)
+                        {
+                            string json_client = JsonUtility.ToJson(client);
+                            response = "{\"msgid\":10002, \"client\":\""+json_client+"\"}";
+                            clients[id].Send(server, response);
+                        }
+                        print("Exit client -> " + ((IPEndPoint)senderRemote).Address.ToString());
+                }
+
+
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+}
